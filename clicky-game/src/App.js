6236b3cf89319563cefa403f941ee94a9bf95ea1
or1979 <orobinson79@gmail.com>
2019-05-13 14:09:46 -0400

    small change to app.js

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,4 +1,4 @@
-import { React, Component } from "react";
+import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 import FriendCard from "./components/FriendCard";
