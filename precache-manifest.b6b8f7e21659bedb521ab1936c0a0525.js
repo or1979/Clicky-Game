bd0c9f6bfae1c64fbbf98a7613775dbfc3bb1a44
or1979 <orobinson79@gmail.com>
2019-05-13 12:27:08 -0400

    Updates

---
+++ precache-manifest.b6b8f7e21659bedb521ab1936c0a0525.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e33dcdc243bb5e0e29c996905e0e867",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "094684271289980a7bed",
+    "url": "/clicky-game/static/css/main.4f8cce75.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "094684271289980a7bed",
+    "url": "/clicky-game/static/js/main.69398d0e.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "7effe5e01fe6b58e96b44d29d33ad354",
+    "url": "/clicky-game/static/media/bowser.7effe5e0.png"
+  },
+  {
+    "revision": "77756d2064554adedd76d28354da9504",
+    "url": "/clicky-game/static/media/daisy.77756d20.png"
+  },
+  {
+    "revision": "ce9931a63d4128d5a0f75012f7ae49a1",
+    "url": "/clicky-game/static/media/donkey-kong.ce9931a6.png"
+  },
+  {
+    "revision": "75b08fd63e132d7e63b2dfafffdf9c73",
+    "url": "/clicky-game/static/media/koopa-troopa.75b08fd6.png"
+  },
+  {
+    "revision": "4d11d5f5deead5e4ecbf1ba2de685207",
+    "url": "/clicky-game/static/media/luigi.4d11d5f5.png"
+  },
+  {
+    "revision": "2e46fb132f568986e285f43c108018ab",
+    "url": "/clicky-game/static/media/mario-background.2e46fb13.gif"
+  },
+  {
+    "revision": "b684a6c73564d7e12c257170f07ccc48",
+    "url": "/clicky-game/static/media/mario.b684a6c7.png"
+  },
+  {
+    "revision": "eaf514a442eb82499f50762a4ff5a239",
+    "url": "/clicky-game/static/media/peach.eaf514a4.png"
+  },
+  {
+    "revision": "3a57962172d31c27f028b3b162e4d9c5",
+    "url": "/clicky-game/static/media/react-logo.3a579621.png"
+  },
+  {
+    "revision": "0d05b16c6a74cae22015c271fa01d9ba",
+    "url": "/clicky-game/static/media/shy-guy.0d05b16c.png"
+  },
+  {
+    "revision": "bd6f7586d91aaf6e1f00fbca5034c732",
+    "url": "/clicky-game/static/media/toad.bd6f7586.png"
+  },
+  {
+    "revision": "eea960ebe0e69a62bb4a71e4e7ec744a",
+    "url": "/clicky-game/static/media/waluigi.eea960eb.png"
+  },
+  {
+    "revision": "e508dfce2af362cba4e11bd0d1f8af8b",
+    "url": "/clicky-game/static/media/wario.e508dfce.png"
+  },
+  {
+    "revision": "1e0c78d4c569b120f4d44120d258d26c",
+    "url": "/clicky-game/static/media/yoshi.1e0c78d4.png"
+  }
+]);
